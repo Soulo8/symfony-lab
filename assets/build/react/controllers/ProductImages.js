32b2b1c9208882b6969d763929469fa2e637fb3b
Soulo8 <soulo08@outlook.com>
2024-09-13 09:43:07 +0200

    Product images add button remove

--- assets/build/react/controllers/ProductImages.js
+++ assets/build/react/controllers/ProductImages.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import ReactSortableJs from "react-sortablejs";
 const ReactSortable = ReactSortableJs.ReactSortable;
-const BasicFunction = ({
-  images
-}) => {
-  const [state, setState] = useState(images);
+const BasicFunction = props => {
+  const [state, setState] = useState(props.images);
+  const handleRemove = index => {
+    setState(prevState => prevState.filter(item => item.index !== index));
+  };
   return /*#__PURE__*/React.createElement(ReactSortable, {
     list: state,
     setList: setState
@@ -12,13 +13,15 @@ const BasicFunction = ({
     key: item.index,
     index: item.index,
     name: item.name,
-    url: item.url
+    url: item.url,
+    onRemove: handleRemove
   })));
 };
 function Image({
   index,
   name,
-  url
+  url,
+  onRemove
 }) {
   return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("input", {
     type: "hidden",
@@ -28,6 +31,8 @@ function Image({
     src: url,
     alt: name,
     width: "150"
-  }));
+  }), /*#__PURE__*/React.createElement("span", {
+    onClick: () => onRemove(index)
+  }, "Supprimer"));
 }
 export default BasicFunction;
--- assets/react/controllers/ProductImages.tsx
+++ assets/react/controllers/ProductImages.tsx
@@ -9,22 +9,27 @@ interface ItemType {
     url: string;
 }
 
-const BasicFunction: FC = ({ images }) => {
-    const [state, setState] = useState<ItemType[]>(images);
+const BasicFunction: FC = (props) => {
+    const [state, setState] = useState<ItemType[]>(props.images);
+
+    const handleRemove = (index) => {
+        setState(prevState => prevState.filter((item) => item.index !== index));
+    };
 
     return (
         <ReactSortable list={state} setList={setState}>
             {state.map((item) => (
-                <Image key={item.index} index={item.index} name={item.name} url={item.url}></Image>
+                <Image key={item.index} index={item.index} name={item.name} url={item.url} onRemove={handleRemove}></Image>
             ))}
         </ReactSortable>
     );
 };
 
-function Image({ index, name, url }) {
+function Image({ index, name, url, onRemove }) {
     return <div>
         <input type="hidden" id={`product_images_${index}_id`} name={`product[images][${index}][id]`} />
         <img src={url} alt={name} width="150" />
+        <span onClick={() => onRemove(index)}>Supprimer</span>
     </div>
 }
 
