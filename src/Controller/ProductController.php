27b478de6ed68a98bc7060785241216ef663e3c9
Soulo8 <soulo08@outlook.com>
2024-09-13 10:41:31 +0200

    Refactoring product images

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -86,19 +86,7 @@ class ProductController extends AbstractController
             $originalImages->add($image);
         }
 
-        if ($request->request->has('product')) {
-            $data = $request->request->all()['product'];
-            if (array_key_exists('images', $data)) {
-                $positions = array_flip(array_keys($data['images']));
-                $images = $product->getImages();
-                foreach ($images as $key => $image) {
-                    if (array_key_exists($key, $positions)) {
-                        $image->setPosition($positions[$key]);
-                    }
-                }
-                $product->sortImagesByPosition();
-            }
-        }
+        $productImageService->updatePosition($request, $product);
 
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
--- src/Service/ProductImageService.php
+++ src/Service/ProductImageService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 class ProductImageService
@@ -27,5 +28,22 @@ class ProductImageService
         }
 
         return $data;
+    }
+
+    public function updatePosition(Request $request, Product $product): void
+    {
+        if ($request->request->has('product')) {
+            $data = $request->request->all()['product'];
+            if (array_key_exists('images', $data)) {
+                $positions = array_flip(array_keys($data['images']));
+                $images = $product->getImages();
+                foreach ($images as $key => $image) {
+                    if (array_key_exists($key, $positions)) {
+                        $image->setPosition($positions[$key]);
+                    }
+                }
+                $product->sortImagesByPosition();
+            }
+        }
     }
 }
