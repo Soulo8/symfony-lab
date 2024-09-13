0dcd1be3601a4295a6a03ac14481d8b167a13f28
Soulo8 <soulo08@outlook.com>
2024-09-13 10:11:14 +0200

    Fix: when all product images have been deleted

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -87,14 +87,17 @@ class ProductController extends AbstractController
         }
 
         if ($request->request->has('product')) {
-            $positions = array_flip(array_keys($request->request->all()['product']['images']));
-            $images = $product->getImages();
-            foreach ($images as $key => $image) {
-                if (array_key_exists($key, $positions)) {
-                    $image->setPosition($positions[$key]);
+            $data = $request->request->all()['product'];
+            if (array_key_exists('images', $data)) {
+                $positions = array_flip(array_keys($data['images']));
+                $images = $product->getImages();
+                foreach ($images as $key => $image) {
+                    if (array_key_exists($key, $positions)) {
+                        $image->setPosition($positions[$key]);
+                    }
                 }
+                $product->sortImagesByPosition();
             }
-            $product->sortImagesByPosition();
         }
 
         $form = $this->createForm(ProductType::class, $product);
