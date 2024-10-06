ed3693f5683579fcd29f120ef25bac91f78b3990
Soulo8 <soulo08@outlook.com>
2024-10-06 15:00:31 +0200

    📦️ Composer require slope-it/breadcrumb-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     TalesFromADev\Twig\Extra\Tailwind\Bridge\Symfony\Bundle\TalesFromADevTwigExtraTailwindBundle::class => ['all' => true],
     TalesFromADev\FlowbiteBundle\TalesFromADevFlowbiteBundle::class => ['all' => true],
+    SlopeIt\BreadcrumbBundle\SlopeItBreadcrumbBundle::class => ['all' => true],
 ];
