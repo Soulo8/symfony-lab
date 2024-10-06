5825d5030d74fae807dcd42738d632c1bf4ac355
Soulo8 <soulo08@outlook.com>
2024-10-06 14:34:48 +0200

    💄 Creation of the design

--- assets/build/react/controllers/ProductImages.js
+++ assets/build/react/controllers/ProductImages.js
@@ -8,7 +8,8 @@ const BasicFunction = props => {
   };
   return /*#__PURE__*/React.createElement(ReactSortable, {
     list: state,
-    setList: setState
+    setList: setState,
+    className: "flex flex-wrap gap-4"
   }, state.map(item => /*#__PURE__*/React.createElement(Image, {
     key: item.index,
     index: item.index,
@@ -31,8 +32,23 @@ function Image({
     src: url,
     alt: name,
     width: "150"
-  }), /*#__PURE__*/React.createElement("span", {
-    onClick: () => onRemove(index)
-  }, "Supprimer"));
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "flex justify-center items-center"
+  }, /*#__PURE__*/React.createElement("svg", {
+    onClick: () => onRemove(index),
+    className: "w-6 h-6 text-gray-800 dark:text-white",
+    "aria-hidden": "true",
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "24",
+    height: "24",
+    fill: "none",
+    viewBox: "0 0 24 24"
+  }, /*#__PURE__*/React.createElement("path", {
+    stroke: "currentColor",
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+    strokeWidth: "2",
+    d: "M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+  }))));
 }
 export default BasicFunction;
--- assets/react/controllers/ProductImages.tsx
+++ assets/react/controllers/ProductImages.tsx
@@ -24,7 +24,7 @@ const BasicFunction: FC = (props) => {
     };
 
     return (
-        <ReactSortable list={state} setList={setState}>
+        <ReactSortable list={state} setList={setState} className="flex flex-wrap gap-4">
             {state.map((item) => (
                 <Image key={item.index} index={item.index} name={item.name} url={item.url} onRemove={handleRemove}></Image>
             ))}
@@ -36,7 +36,11 @@ function Image({ index, name, url, onRemove }: ImageProps) {
     return <div>
         <input type="hidden" id={`product_images_${index}_id`} name={`product[images][${index}][id]`} />
         <img src={url} alt={name} width="150" />
-        <span onClick={() => onRemove(index)}>Supprimer</span>
+        <div className="flex justify-center items-center">
+            <svg onClick={() => onRemove(index)} className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+            </svg>
+        </div>
     </div>
 }
 
