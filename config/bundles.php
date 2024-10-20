4f355d6787dd4dcbad48854e6bebcb8ad8ed35b2
Soulo8 <soulo08@outlook.com>
2024-10-20 10:31:21 +0200

    📦️ Composer require api

--- config/bundles.php
+++ config/bundles.php
@@ -25,4 +25,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
