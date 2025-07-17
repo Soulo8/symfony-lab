4412cd2cbe43341aa8e7acfccf586b335bd723ec
Soulo8 <soulo08@outlook.com>
2025-07-17 16:56:07 +0200

    Refacto React

--- assets/build/react/controllers/ProductImages.js
+++ assets/build/react/controllers/ProductImages.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactSortableJs from "react-sortablejs";
 const ReactSortable = ReactSortableJs.ReactSortable;
-const BasicFunction = props => {
+const ProductImages = props => {
   const [state, setState] = useState(props.images);
   const handleRemove = index => {
     setState(prevState => prevState.filter(item => item.index !== index));
@@ -51,4 +51,4 @@ function Image({
     d: "M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
   }))));
 }
-export default BasicFunction;
+export default ProductImages;
